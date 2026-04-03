069e76ea4c31dc71bf526d958fe2d95e1bd11ec2
AlejandroR <rus25512@uvg.edu.gt>
2026-04-03 16:54:53 -0600

    Paciente.java

---
+++ Paciente.java
@@ -0,0 +1,24 @@
+public class Paciente implements Comparable<Paciente> {
+    private String nombre;
+    private String sintoma;
+    private char prioridad;
+
+    public Paciente(String nombre, String sintoma, char prioridad) {
+        this.nombre = nombre;
+        this.sintoma = sintoma;
+        this.prioridad = prioridad;
+    }
+
+    public String toString() {
+        return nombre + ", " + sintoma + ", " + prioridad;
+    }
+
+    @Override
+    public int compareTo(Paciente otro) {
+        return this.prioridad - otro.prioridad;
+    }
+
+    public char getPrioridad() {
+    return prioridad;
+    }
+}
